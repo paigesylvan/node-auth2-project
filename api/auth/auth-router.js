7643fb32bc8307e940ee6ceca5d009cf10f1b9bd
PaigeSylvan <paigesylvan@gmail.com>
2024-01-11 13:37:27 -0600

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,23 @@
-const router = require("express").Router();
-const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+// Endpoints - Authentication will be implemented using JSON Web Tokens.
 
+const router = require("express").Router();
+const { checkUsernameExists, validateRoleName } = require("./auth-middleware");
+const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require("bcryptjs"); // i think we need this
+const jwt = require("jsonwebtoken"); // i think we need this
+const User = require("../users/users-model");
+
+//  POST /api/auth/register
 router.post("/register", validateRoleName, (req, res, next) => {
+  const { username, password } = req.body;
+  const { role_name } = req;
+  const hash = bcrypt.hashSync(password, 8);
+
+  User.add({ username, password: hash, role_name })
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -16,8 +31,23 @@ router.post("/register", validateRoleName, (req, res, next) => {
    */
 });
 
-
+// POST /api/auth/login
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  // if password is correct, the token must be returned
+  // else return invalid credentials
+
+  const { username, password } = req.body;
+
+  if (bcrypt.compareSync(password, req.user.password)) {
+    const token = buildToken(req.user);
+    res.status(200).json({
+      message: `${username} is back!`,
+      token,
+    });
+  } else {
+    next({ status: 401, message: "Invalid credentials" });
+  }
+
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -38,5 +68,17 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
     }
    */
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
 module.exports = router;
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,26 +1,21 @@
-const db = require('../../data/db-config.js');
+ //start DB Access Functions 
+
+const db = require("../../data/db-config.js");
+
 
 function find() {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
-
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
-   */
+  return db("users as u")
+    .select("user_id", "username", "role_name")
+    .leftJoin("roles as r", "u.role_id", "r.role_id");
 }
 
+
 function findBy(filter) {
+  return db("users as u")
+    .select("user_id", "username", "password", "role_name")
+    .leftJoin("roles as r", "u.role_id", "r.role_id")
+    .where(filter);
+
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
@@ -36,17 +31,13 @@ function findBy(filter) {
    */
 }
 
-function findById(user_id) {
-  /**
-    You will need to join two tables.
-    Resolves to the user with the given user_id.
 
-    {
-      "user_id": 2,
-      "username": "sue",
-      "role_name": "instructor"
-    }
-   */
+function findById(user_id) {
+  return db("users as u")
+    .select("user_id", "username", "role_name")
+    .leftJoin("roles as r", "u.role_id", "r.role_id")
+    .where("user_id", user_id)
+    .first();
 }
 
 /**
@@ -67,21 +58,26 @@ function findById(user_id) {
     "role_name": "team lead"
   }
  */
-async function add({ username, password, role_name }) { // done for you
-  let created_user_id
-  await db.transaction(async trx => {
-    let role_id_to_use
-    const [role] = await trx('roles').where('role_name', role_name)
+async function add({ username, password, role_name }) {
+  // done for you
+  let created_user_id;
+  await db.transaction(async (trx) => {
+    let role_id_to_use;
+    const [role] = await trx("roles").where("role_name", role_name);
     if (role) {
-      role_id_to_use = role.role_id
+      role_id_to_use = role.role_id;
     } else {
-      const [role_id] = await trx('roles').insert({ role_name: role_name })
-      role_id_to_use = role_id
+      const [role_id] = await trx("roles").insert({ role_name: role_name });
+      role_id_to_use = role_id;
     }
-    const [user_id] = await trx('users').insert({ username, password, role_id: role_id_to_use })
-    created_user_id = user_id
-  })
-  return findById(created_user_id)
+    const [user_id] = await trx("users").insert({
+      username,
+      password,
+      role_id: role_id_to_use,
+    });
+    created_user_id = user_id;
+  });
+  return findById(created_user_id);
 }
 
 module.exports = {
